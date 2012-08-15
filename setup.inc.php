db52bacba0dd5f8db37ffd7b45744161c9f4b188
www-data <www-data@azimute2.cedrat.lan>
2012-08-15 11:33:10 +0200

    Revision Lucterios 1.5.2 (pour Coeur 1.4)

--- setup.inc.php
+++ setup.inc.php
@@ -1,24 +1,23 @@
 <?php
-// 	This file is part of Diacamma, a software developped by "Le Sanglier du Libre" (http://www.sd-libre.fr)
-// 	Thanks to have payed a retribution for using this module.
+// This file is part of Diacamma, a software developped by "Le Sanglier du Libre" (http://www.sd-libre.fr)
+// Thanks to have payed a retribution for using this module.
 // 
-// 	Diacamma is free software; you can redistribute it and/or modify
-// 	it under the terms of the GNU General Public License as published by
-// 	the Free Software Foundation; either version 2 of the License, or
-// 	(at your option) any later version.
+// Diacamma is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
 // 
-// 	Diacamma is distributed in the hope that it will be useful,
-// 	but WITHOUT ANY WARRANTY; without even the implied warranty of
-// 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// 	GNU General Public License for more details.
+// Diacamma is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
 // 
-// 	You should have received a copy of the GNU General Public License
-// 	along with Lucterios; if not, write to the Free Software
-// 	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+// You should have received a copy of the GNU General Public License
+// along with Lucterios; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 // 
-// 		Contributeurs: Fanny ALLEAUME, Pierre-Olivier VERSCHOORE, Laurent GAY
-// setup file write by SDK tool
-// --- Last modification: Date 10 November 2011 20:13:28 By  ---
+// Contributeurs: Fanny ALLEAUME, Pierre-Olivier VERSCHOORE, Laurent GAY
+// setup file write by Lucterios SDK tool
 
 $extention_name="applis";
 $extention_description="Produit Lucterios standard";
@@ -29,7 +28,7 @@ $extension_libre=true;
 
 $version_max=1;
 $version_min=5;
-$version_release=1;
+$version_release=2;
 $version_build=5;
 
 $depencies=array();
@@ -43,5 +42,6 @@ $actions=array();
 $params=array();
 
 $extend_tables=array();
+$signals=array();
 
 ?>
